6aff7216fea49cd4d7633136bee5cdcbc4f6a923
长寿 <1369548156@qq.com>
2024-07-25 10:20:36 +0800

    feat: support css hmr

--- src/node/plugins/css.ts
+++ src/node/plugins/css.ts
@@ -1,9 +1,16 @@
 import { readFile } from "fs-extra";
 import { Plugin } from "../plugin";
+import { CLIENT_PUBLIC_PATH } from "../constants";
+import { getShortName, normalizePath } from "../utils";
+import { ServerContext } from "../server";
 
 export function cssPlugin(): Plugin {
+  let severContext: ServerContext;
   return {
     name: "t-vite:css",
+    configureServer(s) {
+      severContext = s;
+    },
     // 读取文件内容
     load(id) {
       if (id.endsWith("css")) {
@@ -14,12 +21,20 @@ export function cssPlugin(): Plugin {
     async transform(code, id) {
       if (id.endsWith("css")) {
         const jsContent = `
+import { createHotContext as __vite__createHotContext } from "${CLIENT_PUBLIC_PATH}";
+import.meta.hot = __vite__createHotContext("/${getShortName(
+          normalizePath(id),
+          normalizePath(severContext.root)
+        )}");
+import { updateStyle, removeStyle } from "${CLIENT_PUBLIC_PATH}";
+
+const id = '${id}';
 const css = \`${code.replace(/\n/g, "")}\`;
-const style = document.createElement("style");
-style.setAttribute("type", "text/css");
-style.innerHTML = css;
-document.head.appendChild(style);
-export default css;`.trim();
+
+updateStyle(id, css);
+import.meta.hot.accept();
+export default css;
+import.meta.hot.prune(() => removeStyle(id));`.trim();
         return {
           code: jsContent,
         };
